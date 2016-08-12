c307b9a9287b8fc13d8cac49df7a583c6c4f9ba2
Matthieu Auger <matthieua@theodo.fr>
2016-08-12 19:17:48 +0200

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+// Images
+require('file?name=[name].[ext]!./trello-scrum-icon.png');
+require('file?name=/images/[name].[ext]!./images/points-done.png');
+require('file?name=/images/[name].[ext]!./images/storypoints-icon.png');
+require('file?name=/images/[name].[ext]!./images/trello-scrum-icon_12x12.png');
+require('file?name=/images/[name].[ext]!./images/trello-scrum-icon_18x18.png');
+
+// HTML
+require('file?name=[name].[ext]!./help.html');
+
+// CSS
+require('file?name=[name].[ext]!./trelloscrum.css');
+
+// json
+require('file?name=[name].[ext]!./manifest.json');
+
+// js
+require('file?name=[name].[ext]!./trelloscrum.js');
+require('file?name=[name].[ext]!./jquery-2.1.4.min.js');
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+var config = {
+    entry: './src',               // entry point
+    output: {                     // output folder
+        path: './dist',           // folder path
+        filename: 'app.js'     // file name
+    }
+};
+module.exports = config;
